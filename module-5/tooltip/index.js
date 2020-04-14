0a528808891cbd406972a0186bfd54c54e22a357
n.zbiranik <n.zbiranik@cdek.ru>
2020-04-14 16:33:07 +0700

    Module 5: home task - Tooltip

--- module-5/tooltip/index.js
+++ module-5/tooltip/index.js
@@ -9,22 +9,47 @@ class Tooltip {
     }
 
     Tooltip.instance = this;
+    this.onHover = this.onHover.bind(this);
+    this.offHover = this.offHover.bind(this)
   }
 
   initEventListeners () {
-
+    document.addEventListener('mouseover', this.onHover);
+    document.addEventListener('mouseout', this.offHover);
   }
 
   initialize () {
+    this.render();
+    this.initEventListeners();
+  }
 
+  onHover (event) {
+    const tooltipElement = event.target;
+
+    if (tooltipElement.dataset.tooltip != undefined) {
+      this.element.innerHTML = tooltipElement.dataset.tooltip;
+      const left = event.clientX || 0;
+      const top = event.clientY || 0;
+      this.element.style = `left: ${left}px; top: ${top}px;`;
+      this.element.hidden = false;
+    }
+  }
+
+  offHover (event) {
+    this.element.hidden = true;
   }
 
   render () {
-
+    const element = document.createElement('div');
+    element.classList.add('tooltip');
+    this.element = element;
+    this.element.hidden = true;
+    document.body.prepend(this.element);
   }
 
   destroy () {
-
+    document.removeEventListener('mouseover', this.onHover);
+    document.removeEventListener('mouseout', this.offHover);
   }
 }
 
