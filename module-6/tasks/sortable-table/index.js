f2466c294d0f5362c34189b1987bf6837f872852
n.zbiranik <n.zbiranik@cdek.ru>
2020-04-16 23:51:42 +0700

    Module 6: home task

--- module-6/tasks/sortable-table/index.js
+++ module-6/tasks/sortable-table/index.js
@@ -8,8 +8,23 @@ export default class SortableTable {
   data = [];
   pageSize = 30;
 
-  onSortClick = event => {
-    // ...logic
+  onSortClick = async (event) => {
+    const element = event.target.closest('.sortable-table__cell');
+    const columnId = element.dataset.name || '';
+    const sortable = (element.dataset.sortable || '') === 'true';
+    const order = this.changeDirection(columnId);
+    if (sortable) {
+      this.subElements.loading.style.display = 'grid';
+      const data = this.isSortLocally ? this.sortData(columnId, order) : await this.loadData(columnId, order);
+      element.dataset.order = order;
+      this.sorted = {
+        id: columnId,
+        order: order
+      };
+
+      this.renderRows(data);
+      this.subElements.loading.style.display = 'none';
+    }
   };
 
   constructor(headersConfig = [], {
@@ -29,6 +44,50 @@ export default class SortableTable {
     this.render();
   }
 
+  getTable () {
+    let result = '<div class="sortable-table"><div class="sortable-table__header sortable-table__row" data-elem="header">';
+    for (let headOne of this.headersConfig) {
+      result += `<div class="sortable-table__cell" data-name="${headOne.id}" data-sortable="${headOne.sortable}" data-order="${(headOne.id === this.sorted.id) ? this.sorted.order : ''}">
+        <span>${headOne.title}</span>
+        <span class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>
+      </div>`;
+    }
+    result += `</div>
+    <div data-elem="body" class="sortable-table__body"></div>
+    <div data-elem="loading" class="loading-line sortable-table__loading-line"></div>
+    <div data-elem="emptyPlaceholder" class="sortable-table__empty-placeholder">
+      <div>
+        <p>No products satisfies your filter criteria</p>
+        <button type="button" class="button-primary-outline">Reset all filters</button>
+      </div>
+    </div>
+    </div>`;
+    return result;
+  }
+
+  renderRows (data) {
+    this.subElements.emptyPlaceholder.style.display = 'none';
+    let result = '';
+    for (let dataOne of data) {
+      result += '<div class="sortable-table__row" data-element="">';
+      for (let headOne of this.headersConfig) {
+        if (headOne.hasOwnProperty('template')) {
+          result += `<div class="sortable-table__cell" data-element="">${headOne.template(dataOne[headOne.id] || '')}</div>`;
+        } else {
+          result += `<div class="sortable-table__cell" data-element="">${dataOne[headOne.id] || ''}</div>`;
+        }
+      }
+      result += '</div>';
+    }
+
+    this.subElements.body.innerHTML = result;
+    if ((data.length || 0) === 0) {
+      this.subElements.emptyPlaceholder.style.display = 'flex';
+    }
+  }
+
   async render() {
     const {id, order} = this.sorted;
     const wrapper = document.createElement('div');
@@ -36,25 +95,42 @@ export default class SortableTable {
     wrapper.innerHTML = this.getTable();
 
     const element = wrapper.firstElementChild;
-
-    this.element = element;
     this.subElements = this.getSubElements(element);
 
+    this.element = element;
+    this.subElements.loading.style.display = 'grid';
+
     const data = await this.loadData(id, order);
+    this.data = data;
 
     this.renderRows(data);
+    this.subElements.loading.style.display = 'none';
 
     this.initEventListeners();
   }
 
   async loadData (id, order) {
-    // ... logic
-
-    return data;
+    this.url.searchParams.append('_sort', id);
+    this.url.searchParams.append('_order', order);
+    return fetchJson(this.url, {});
   }
 
   initEventListeners () {
     this.subElements.header.addEventListener('pointerdown', this.onSortClick);
+  }
+
+  changeDirection (columnId) {
+    let oldOrder = '';
+    if (this.sorted.id === columnId) {
+      oldOrder = this.sorted.order;
+    }
+    let newOrder = '';
+    switch (oldOrder) {
+      case 'asc': newOrder = 'desc'; break;
+      case 'desc': newOrder = 'asc'; break;
+      default: newOrder = 'asc'; break;
+    }
+    return newOrder;
   }
 
   sortData (id, order) {
@@ -78,10 +154,10 @@ export default class SortableTable {
   }
 
   getSubElements(element) {
-    const elements = element.querySelectorAll('[data-element]');
+    const elements = element.querySelectorAll('[data-elem]');
 
     return [...elements].reduce((accum, subElement) => {
-      accum[subElement.dataset.element] = subElement;
+      accum[subElement.dataset.elem] = subElement;
 
       return accum;
     }, {});
